4facfeb6443fbfdf1c7b907a5cb875c4bc45adb0
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2018-12-23 14:13:02 +0000

    update gitignore

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-  const browser = await puppeteer.launch();
+//   const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+    headless: false
+  });
   const page = await browser.newPage();
   
   await page.goto('https://github.com');
